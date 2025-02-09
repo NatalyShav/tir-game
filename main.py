83f896a564b1767a96e37f291598f49cfa3077a0
NatalyShav <shaverinanatalia@gmail.com>
2025-02-09 22:20:57 +0300

    add cycle code

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ pygame.display.set_caption("Игра Тир")
 icon = pygame.image.load("image/icon.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("image/БМВ АЙ8.png")
-target_width = 80
+target_img = pygame.image.load("image/БМВ АЙ8 мини.png.")
+target_width = 131
 target_heigth = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGTH - target_heigth)
@@ -22,7 +22,18 @@ color = (random.randint(0,255), random.randint(0,255),random.randint(0,255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_heigth:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGTH - target_heigth)
+
+    screen.blit(target_img,(target_x,target_y))
+    pygame.display.update()
 
 
 pygame.quit()
